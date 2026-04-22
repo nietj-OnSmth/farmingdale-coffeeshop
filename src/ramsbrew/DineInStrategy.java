a5779fdf448e4e41c115260e8d2f47ea605df12c
claudiagonz232-web <claudiagonza232@gmail.com>
2026-04-22 00:40:56 -0400

    Implements order type strategy java and outputs dine-in if it's for dine in.

--- src/ramsbrew/DineInStrategy.java
+++ src/ramsbrew/DineInStrategy.java
@@ -1,4 +1,10 @@
 package ramsbrew;
 
-public class DineInStrategy {
+// Strategy class for dine-in orders
+public class DineInStrategy implements OrderTypeStrategy {
+
+    // Shows that the order is dine-in
+    public void handleOrderType() {
+        System.out.println("Order Type: Dine-In");
+    }
 }
