9cb49d00a84f6e8497f553c45a6e411ee61d267d
claudiagonz232-web <claudiagonza232@gmail.com>
2026-04-22 00:04:25 -0400

    i added mobile pickup code from line 3 to 9

--- src/ramsbrew/MobilePickupStrategy.java
+++ src/ramsbrew/MobilePickupStrategy.java
@@ -1,4 +1,10 @@
 package ramsbrew;
 
-public class MobilePickupStrategy {
-}
+public class MobilePickupStrategy implements OrderTypeStrategy {
+
+    // This shows that the order is mobile pickup
+        public void handleOrderType() {
+            System.out.println("Order Type: Mobile Pickup");
+        }
+    }
+
