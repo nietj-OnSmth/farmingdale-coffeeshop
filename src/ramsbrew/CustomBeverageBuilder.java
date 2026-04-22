9b5422dec21a879aa6eef8933f172a4146e8eec2
claudiagonz232-web <claudiagonza232@gmail.com>
2026-04-22 00:46:27 -0400

    This sets how the beverage will be perzonalized.

--- src/ramsbrew/CustomBeverageBuilder.java
+++ src/ramsbrew/CustomBeverageBuilder.java
@@ -1,4 +1,45 @@
 package ramsbrew;
+//Concrete builder class that customizes a beverage
+public class CustomBeverageBuilder implements BeverageBuilder {
+    private Beverage beverage;
 
-public class CustomBeverageBuilder {
+    // Constructor receives the beverage to customize
+    public CustomBeverageBuilder(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    // Sets the beverage temperature and returns the builder
+    public BeverageBuilder setTemperature(String temperature) {
+        beverage.setTemperature(temperature);
+        return this;
+    }
+
+    // Sets the beverage size and returns the builder
+    public BeverageBuilder setSize(String size) {
+        beverage.setSize(size);
+        return this;
+    }
+
+    // Sets the milk type and returns the builder
+    public BeverageBuilder setMilk(String milk) {
+        beverage.setMilk(milk);
+        return this;
+    }
+
+    // Sets the number of shots and returns the builder
+    public BeverageBuilder setShots(int shots) {
+        beverage.setShots(shots);
+        return this;
+    }
+
+    // Sets the sweetener and returns the builder
+    public BeverageBuilder setSweetener(String sweetener) {
+        beverage.setSweetener(sweetener);
+        return this;
+    }
+
+    // Returns the finished customized beverage
+    public Beverage build() {
+        return beverage;
+    }
 }
