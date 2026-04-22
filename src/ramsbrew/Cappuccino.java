34a7a1f73b69fafa556eec8a87281bda6b891ab2
kaleysantonocito <kaleysantonocito31@gmail.com>
2026-04-21 22:07:53 -0400

    Cappuccino class

--- src/ramsbrew/Cappuccino.java
+++ src/ramsbrew/Cappuccino.java
@@ -1,4 +1,12 @@
 package ramsbrew;
 
-public class Cappuccino {
+// Cappuccino is a specific type of Beverage
+public class Cappuccino extends Beverage {
+
+    // Calls the Beverage constructor and gives Cappuccino 1 default shot
+    public Cappuccino() {
+        super("Cappuccino");
+        this.shots = 1;
+    }
 }
+
