e8c1c6bf30c8369847843a0cb2b53ccd69e1f124
kaleysantonocito <kaleysantonocito31@gmail.com>
2026-04-21 22:08:16 -0400

    Latte class

--- src/ramsbrew/Latte.java
+++ src/ramsbrew/Latte.java
@@ -1,4 +1,11 @@
 package ramsbrew;
 
-public class Latte {
+// Latte is a specific type of Beverage
+public class Latte extends Beverage {
+
+    // Calls the Beverage constructor and gives Latte 1 default shot
+    public Latte() {
+        super("Latte");
+        this.shots = 1;
+    }
 }
