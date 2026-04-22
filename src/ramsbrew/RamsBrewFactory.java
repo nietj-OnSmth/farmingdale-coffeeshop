c41bbbef46ddc9426f605e0c0468131312c6a57a
claudiagonz232-web <claudiagonza232@gmail.com>
2026-04-22 00:38:09 -0400

    Create and return Beverages that customers want. If choices aren't there null is returned

--- src/ramsbrew/RamsBrewFactory.java
+++ src/ramsbrew/RamsBrewFactory.java
@@ -1,4 +1,19 @@
 package ramsbrew;
 
-public class RamsBrewFactory {
-}
+public class RamsBrewFactory implements BeverageFactory {
+
+        // Creates and returns the right beverage type
+        public Beverage createBeverage(String type) {
+            if (type.equalsIgnoreCase("Coffee")) {
+                return new Coffee();
+            } else if (type.equalsIgnoreCase("Latte")) {
+                return new Latte();
+            } else if (type.equalsIgnoreCase("Cappuccino")) {
+                return new Cappuccino();
+            } else if (type.equalsIgnoreCase("Tea")) {
+                return new Tea();
+            } else {
+                return null; // Returns null if the type is invalid
+            }
+        }
+    }
