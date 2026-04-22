a6a64aa4887e427f956e994175410bac0121e886
claudiagonz232-web <claudiagonza232@gmail.com>
2026-04-22 00:49:24 -0400

    Builds the beverage based on temperature and size as well as ingredients

--- src/ramsbrew/BeverageBuilder.java
+++ src/ramsbrew/BeverageBuilder.java
@@ -1,4 +1,22 @@
 package ramsbrew;
 
-public class BeverageBuilder {
-}
+    public interface BeverageBuilder {
+
+        // Sets the temperature of the beverage
+        BeverageBuilder setTemperature(String temperature);
+
+        // Sets the size of the beverage
+        BeverageBuilder setSize(String size);
+
+        // Sets the milk type of the beverage
+        BeverageBuilder setMilk(String milk);
+
+        // Sets the number of shots
+        BeverageBuilder setShots(int shots);
+
+        // Sets the sweetener type
+        BeverageBuilder setSweetener(String sweetener);
+
+        // Returns the finished beverage object
+        Beverage build();
+    }
